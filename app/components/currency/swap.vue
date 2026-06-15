8fccbc286efefaa5d939846016cd33b25a8f864d
Eli Silk <37377295+elisilk@users.noreply.github.com>
2026-06-15 17:22:25 -0400

    setup initial swap component

---
+++ app/components/currency/swap.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const base = defineModel("base", { type: String });
+const quote = defineModel("quote", { type: String });
+
+function swap() {
+  [base.value, quote.value] = [quote.value, base.value];
+}
+</script>
+
+<template>
+  <button @click="swap">
+    swap
+  </button>
+</template>
