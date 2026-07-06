c42210c8ae1cf548983fc15d986032ea4328bf2e
Eli Silk <37377295+elisilk@users.noreply.github.com>
2026-07-06 09:43:20 -0400

    tabs as select menu on mobile

--- app/components/section/tabs.vue
+++ app/components/section/tabs.vue
@@ -27,15 +27,53 @@ const items = computed<TabsItem[]>(() => [
     badge: exchange.conversionLog.length,
   },
 ]);
+
+const activeItemBadge = computed(() => {
+  return items.value.find(item => item.value === exchange.activeTab)?.badge;
+});
 </script>
 
 <template>
+  <USelectMenu
+    v-model="exchange.activeTab"
+    value-key="value"
+    :items
+    :search-input="false"
+    class="sm:hidden w-full"
+    :ui="{
+      content: 'bg-muted',
+      viewport: 'pt-0',
+      base: 'uppercase bg-muted border-accented',
+      itemLabel: 'uppercase flex gap-2 items-center',
+    }"
+  >
+    <template #default="{ modelValue }">
+      {{ modelValue }}
+      <UBadge
+        v-if="activeItemBadge"
+        class="ring-0 inline-flex rounded-full bg-primary-200 dark:bg-primary-800 w-5 h-5 items-center justify-center text-xs text-primary"
+      >
+        {{ activeItemBadge }}
+      </UBadge>
+    </template>
+    <template #item-label="{ item }">
+      {{ item.label }}
+      <UBadge
+        v-if="item.badge"
+        class="ring-0 inline-flex rounded-full bg-primary-200 dark:bg-primary-800 w-5 h-5 items-center justify-center text-xs text-primary"
+      >
+        {{ item.badge }}
+      </UBadge>
+    </template>
+  </USelectMenu>
+
   <UTabs
     v-model="exchange.activeTab"
     :items
     variant="link"
     :unmount-on-hide="false"
     class="gap-4 md:gap-5"
+    :ui="{ list: 'hidden sm:flex' }"
   >
     <template #history>
       <SectionHistory />
