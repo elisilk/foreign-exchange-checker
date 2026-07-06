b2cad05c0a30f4c7e0508e08e4012999ef8cfd01
Eli Silk <37377295+elisilk@users.noreply.github.com>
2026-07-06 10:19:15 -0400

    setup seo meta data, favicons, and share image

--- app/components/util/icon-viewer.vue
+++ app/components/util/icon-viewer.vue
@@ -6,16 +6,16 @@
 
     <UCard>
       <div class="flex flex-wrap gap-4 justify-center">
-        <img src="/icon-arrow-right.svg" alt="arrow right">
-        <img src="/icon-check.svg" alt="check">
-        <img src="/icon-chevron-down.svg" alt="chevron down">
-        <img src="/icon-delete-filled.svg" alt="delete filled">
-        <img src="/icon-delete.svg" alt="delete">
-        <img src="/icon-exchange-vertical.svg" alt="exchange vertical">
-        <img src="/icon-exchange.svg" alt="exchange">
-        <img src="/icon-search.svg" alt="search">
-        <img src="/icon-star-filled.svg" alt="star filled">
-        <img src="/icon-star.svg" alt="star">
+        <img src="/icons/icon-arrow-right.svg" alt="arrow right">
+        <img src="/icons/icon-check.svg" alt="check">
+        <img src="/icons/icon-chevron-down.svg" alt="chevron down">
+        <img src="/icons/icon-delete-filled.svg" alt="delete filled">
+        <img src="/icons/icon-delete.svg" alt="delete">
+        <img src="/icons/icon-exchange-vertical.svg" alt="exchange vertical">
+        <img src="/icons/icon-exchange.svg" alt="exchange">
+        <img src="/icons/icon-search.svg" alt="search">
+        <img src="/icons/icon-star-filled.svg" alt="star filled">
+        <img src="/icons/icon-star.svg" alt="star">
       </div>
     </UCard>
   </section>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,4 +30,45 @@ export default defineNuxtConfig({
       standalone: false,
     },
   },
+
+  /* app config */
+  app: {
+    head: {
+      titleTemplate: "Foreign Exchange Checker - %s",
+      title: "Foreign Exchange Checker",
+      htmlAttrs: {
+        lang: "en",
+      },
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        { rel: "manifest", href: "/site.webmanifest" },
+      ],
+      meta: [
+        {
+          name: "description",
+          content: "To compare exchange rates for currencies.",
+        },
+        {
+          property: "og:description",
+          content: "To compare exchange rates for currencies.",
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          property: "og:image",
+          content: "https://foreign-exchange-checker-seven.vercel.app/foreign-exchange-checker-share-image.png",
+        },
+        {
+          property: "og:image:width",
+          content: "1200",
+        },
+        {
+          property: "og:image:height",
+          content: "630",
+        },
+      ],
+    },
+  },
 });
